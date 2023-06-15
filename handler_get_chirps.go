ba3fa16a2c9e17071f771ae0a24c42f072640d5c
ivica <blizujemars@gmail.com>
2023-06-15 12:25:16 +0200

    add get chirps handler

---
+++ handler_get_chirps.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sort"
+)
+
+func (cfg *appConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request) {
+	chirps, err := cfg.database.GetChirps()
+	if err != nil {
+		log.Printf("Something went wrong: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].Id < chirps[j].Id
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(chirps)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(dat)
+}
