f2574dbc1151ae4e7fab3f88eac779cc738ae1d9
ivica <blizujemars@gmail.com>
2023-06-15 11:59:53 +0200

    add CreateChirp, loadDb, writeDb

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/fs"
@@ -19,31 +20,53 @@ type DBStructure struct {
 
 type Chirp struct {
 	Id   int    `json:"id"`
-	Body string `json:"cleaned_body"`
+	Body string `json:"body"`
 }
 
 func NewDb(path string) (*DB, error) {
+	db := &DB{
+		path: path,
+	}
 	_, err := os.ReadFile(path)
-	db := &DB{}
 
 	if errors.Is(err, fs.ErrNotExist) {
-		err = db.ensureDB(path)
+		err = db.ensureDB()
 		if err != nil {
 			return db, err
 		}
-	} else {
-		return db, nil
+	}
+	if err != nil {
+		return db, err
 	}
 
 	return db, nil
 }
 
-// func (db *DB) CreateChirp(body string) (Chirp, error)
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	// load db
+	DBStruct, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	id := len(DBStruct.Chirps) + 1
+	newChirp := Chirp{
+		Id:   id,
+		Body: body,
+	}
+	DBStruct.Chirps[id] = newChirp
+	// write db
+	err = db.writeDB(DBStruct)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return newChirp, nil
+}
 
 // func (db *DB) GetChirps() ([]Chirp, error)
 
-func (db *DB) ensureDB(path string) error {
-	_, err := os.Create(path)
+func (db *DB) ensureDB() error {
+	_, err := os.Create(db.path)
 	if err != nil {
 		return err
 	}
@@ -51,6 +74,30 @@ func (db *DB) ensureDB(path string) error {
 	return nil
 }
 
-// func (db *DB) loadDB() (DBStructure, error)
+func (db *DB) loadDB() (DBStructure, error) {
+	dbFile, err := os.ReadFile(db.path)
+	dbStructure := DBStructure{}
+	if err != nil {
+		return dbStructure, err
+	}
+	err = json.Unmarshal(dbFile, &dbStructure)
+	if err != nil {
+		return dbStructure, err
+	}
 
-// func (db *DB) writeDB(dbStructure DBStructure) error
+	return dbStructure, nil
+}
+
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	dbJson, err := json.Marshal(dbStructure)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("database.json", dbJson, 0o666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
