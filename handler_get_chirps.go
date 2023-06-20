244db19bd8f36044b2a64c3e60ef872b1249c2cc
ivica <blizujemars@gmail.com>
2023-06-20 17:48:07 +0200

    add get chirps by author id

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -3,9 +3,32 @@ package main
 import (
 	"net/http"
 	"sort"
+	"strconv"
 )
 
 func (cfg *appConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request) {
+	authorId := req.URL.Query().Get("author_id")
+
+	if authorId != "" {
+		id, err := strconv.Atoi(authorId)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		chirps, err := cfg.database.GetChirpsByAuthor(id)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps.")
+			return
+		}
+
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].Id < chirps[j].Id
+		})
+
+		respondWithJSON(w, http.StatusOK, chirps)
+		return
+	}
+
 	chirps, err := cfg.database.GetChirps()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps.")
--- internal/db/get_all_chirps.go
+++ internal/db/get_all_chirps.go
@@ -23,3 +23,24 @@ func (db *DB) GetChirps() ([]Chirp, error) {
 
 	return chirps, nil
 }
+
+func (db *DB) GetChirpsByAuthor(id int) ([]Chirp, error) {
+	dbFile, err := os.ReadFile(db.path)
+	dbStructure := DBStructure{}
+	if err != nil {
+		return []Chirp{}, err
+	}
+	err = json.Unmarshal(dbFile, &dbStructure)
+	if err != nil {
+		return []Chirp{}, err
+	}
+	chirps := []Chirp{}
+
+	for _, val := range dbStructure.Chirps {
+		if val.AuthorId == id {
+			chirps = append(chirps, val)
+		}
+	}
+
+	return chirps, nil
+}
