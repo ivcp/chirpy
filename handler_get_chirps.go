3b69411c9e73c4f655e4db61a6feff3324ef2233
ivica <blizujemars@gmail.com>
2023-06-21 13:39:49 +0200

    refactor

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 	"sort"
 	"strconv"
+
+	"github.com/ivcp/chirpy/internal/db"
 )
 
 func (cfg *appConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request) {
 	authorId := req.URL.Query().Get("author_id")
+	sortParam := req.URL.Query().Get("sort")
+	sortDirection := "asc"
+
+	if sortParam != "" && sortParam != "asc" && sortParam != "desc" {
+		respondWithError(w, http.StatusBadRequest, "Invalid sort input")
+		return
+	}
+
+	if sortParam == "desc" {
+		sortDirection = "desc"
+	}
 
 	if authorId != "" {
 		id, err := strconv.Atoi(authorId)
@@ -21,9 +34,7 @@ func (cfg *appConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request)
 			return
 		}
 
-		sort.Slice(chirps, func(i, j int) bool {
-			return chirps[i].Id < chirps[j].Id
-		})
+		sortChirps(chirps, sortDirection)
 
 		respondWithJSON(w, http.StatusOK, chirps)
 		return
@@ -35,9 +46,16 @@ func (cfg *appConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request)
 		return
 	}
 
-	sort.Slice(chirps, func(i, j int) bool {
-		return chirps[i].Id < chirps[j].Id
-	})
+	sortChirps(chirps, sortDirection)
 
 	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func sortChirps(chirps []db.Chirp, dir string) {
+	sort.Slice(chirps, func(i, j int) bool {
+		if dir == "desc" {
+			return chirps[i].Id > chirps[j].Id
+		}
+		return chirps[i].Id < chirps[j].Id
+	})
 }
